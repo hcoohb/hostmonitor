a5172acefd1aedf1ce42251c1292fd21efd3cb57
hcoohb <hcoohb@gmail.com>
2021-12-28 18:50:31 +1000

    add user probe

---
+++ hostmonitor/probes/user.py
@@ -0,0 +1,31 @@
+import subprocess
+from typing import Dict
+
+from plugins import register_class
+
+from probes._probe import Probe
+
+
+@register_class
+class User(Probe):
+    def run(self) -> Dict:
+        """Collect user data"""
+        num_users = "-1"
+        num_sessions = "-1"
+        try:
+            comm = "users |tr ' ' '\n' | sort -u | wc -l"
+            num_users = (
+                subprocess.check_output([comm], stderr=subprocess.STDOUT, shell=True)
+                .decode("utf-8")
+                .rstrip()
+            )
+            comm = "who | wc -l"
+            num_sessions = (
+                subprocess.check_output([comm], stderr=subprocess.STDOUT, shell=True)
+                .decode("utf-8")
+                .rstrip()
+            )
+        except:
+            pass
+        ret_dict = {"num_users": num_users, "num_sessions": num_sessions}
+        return ret_dict
