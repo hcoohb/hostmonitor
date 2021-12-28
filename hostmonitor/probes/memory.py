7f9c84d16a924811ff7c8c9251dd4009e17d273d
hcoohb <hcoohb@gmail.com>
2021-12-28 18:39:05 +1000

    add memory probe

---
+++ hostmonitor/probes/memory.py
@@ -0,0 +1,22 @@
+import psutil
+from typing import Dict
+
+from plugins import register_class
+
+from probes._probe import Probe
+
+
+@register_class
+class SysInfo(Probe):
+    def run(self) -> Dict:
+        """Collect memory info"""
+        memory_stats = psutil.virtual_memory()
+        memory_total = memory_stats.total
+        memory_used = memory_stats.used
+        memory_used_percent = memory_stats.percent
+        ret_dict = {
+            "total": memory_total,
+            "used": memory_used,
+            "used_pct": memory_used_percent,
+        }
+        return ret_dict
